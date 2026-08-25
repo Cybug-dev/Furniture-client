049be59dd9f6879c20641d983ccb724f80aa92c7
cybug <enwafor008@gmail.com>
2026-08-25 01:01:37 +0100

    refactor(banner): rename HeroBanner to FeatureBanner
    Rename the banner component and update its related imports, styles, and
    references to use the new FeatureBanner name.

--- src/components/FeatureBanner/FeatureSkeleton.jsx
+++ src/components/FeatureBanner/FeatureSkeleton.jsx
@@ -2,12 +2,12 @@
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
-export default function HeroSkeleton() {
+export default function FeatureSkeleton() {
   return (
-    <div className="hero-skeleton" aria-hidden="true">
+    <div className="feature-skeleton" aria-hidden="true">
       <SkeletonTheme baseColor="#e6e8ec" highlightColor="#f7f8fa">
-        <div className="hero-skeleton__stage">
-          <div className="hero-skeleton__copy">
+        <div className="feature-skeleton__stage">
+          <div className="feature-skeleton__copy">
             <Skeleton width={110} height={28} borderRadius={50} />
             <Skeleton width="55%" height={48} style={{ marginTop: 16 }} />
             <Skeleton width="42%" height={18} style={{ marginTop: 12 }} count={2} />
