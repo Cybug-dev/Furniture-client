481a7474e54702e5853cf0a5cf9db288112afd4b
cybug <enwafor008@gmail.com>
2026-09-18 01:18:04 +0100

    Revert "Merge pull request #19 from Cybug-dev/emmanuel"
    This reverts commit 021f0ceaf60a31fea711c08708da3579f6d0afe8, reversing
    changes made to d6eca3386f667ac9e0fe6749955bf2015e84dc32.

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,15 @@ import FeatureBanner from "./components/FeatureBanner/FeatureBanner";
 import FlashSale from './components/FlashSale';
 import Carousel from './components/Carousel/Carousel'
 import Footer from './components/Footer/Footer';
-import { AuthPage } from './auth/Auth';
-import { BrowserRouter, Routes, Route } from 'react-router';
 
-function Home() {
+function App() {
   return (
     <div className="app-shell">
       <Header />
       <main className="main-content">
         <Hero />
         <Browse />
-        <Carousel />
+        <Carousel/>
         <FeatureBanner />
         <ShareSetup />
         <FlashSale />
@@ -24,17 +22,6 @@ function Home() {
       </main>
     </div>
   )
-}
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<AuthPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
 }
 
 export default App
